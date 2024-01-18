d4429d9f44e6162dde6d6a725b0c01ba27995aaf
Sunil Kumar Choubey <sunilkumar.choubey@gartner.com>
2024-01-18 23:22:42 +0530

    Added the message

---
+++ gcd.py
@@ -0,0 +1,30 @@
+import os
+import math
+
+
+def computegcd(n1,n2):
+    if n2> n1 :
+        n1,n2 = n2 , n1 
+
+    if n1 == n2:
+        return n1 
+    
+    if n1 == 0 or n2 ==0:
+        return 0
+    
+    if n1 ==1 or n2 ==1 :
+        return 1
+    
+    ###if n2 divides n1 then n2 is the gcd
+    if n1%n2 ==0 :
+        return n2 
+    else:
+        ##otherwise as per euclid we will search for the diff  and try to find the gcd between the smallest 
+        ## of the the two numbers and the difference between the two numbers. 
+        diff = n1 - n2 
+        return computegcd(n2 , diff)
+    
+if __name__ == "__main__":
+    first_num = int(input("Enter the first input:"))
+    Second_num = int(input("Enter the Second input:"))
+    print(f" GCD is {computegcd(first_num,Second_num)}")
